7471713bb0d29d1d0cc8fe96a89a6f98f21c20b8
yusera-ansari-at-git <142092189+yusera-ansari-at-git@users.noreply.github.com>
2023-10-08 18:07:56 +0530

    added meal items -lesson:160

--- lib/src/screens/meals.dart
+++ lib/src/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/src/models/meal.dart';
+import 'package:meals/src/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -10,10 +11,7 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (_, indx) => Text(
-        meals[indx].title,
-        style: TextStyle(color: Colors.white),
-      ),
+      itemBuilder: (_, indx) => MealItem(meal: meals[indx]),
     );
 
     if (meals.isEmpty) {
---
+++ lib/src/widgets/meal_item.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:meals/src/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+  final Meal meal;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      clipBehavior: Clip.hardEdge,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      margin: const EdgeInsets.all(8),
+      child: InkWell(
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.black54,
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 44),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      children: [],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
